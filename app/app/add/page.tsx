ee50d1af0d70b0a55732bfdb22ad08c923698dcf
PabloF <pablofitoussi@gmail.com>
2025-07-23 17:09:24 +0200

    handle added recently

--- app/app/add/page.tsx
+++ app/app/add/page.tsx
@@ -6,18 +6,52 @@ import WordCard from "@/components/block/wordCard"
 import CardSkeleton from "@/components/ui/cardSkeleton"
 
 export default function Words() {
+  const [addedWord, setAddedWord] = useState<[] | WordType[]>([])
   const [translatedWord, setTranslatedWord] = useState<null | WordType>(null)
+  const [isLoading, setIsLoading] = useState(false)
 
-  const handleTranslatedWord = (word: WordType | null) => {
+  function handleTranslatedWord(word: WordType | null) {
     setTranslatedWord(word)
-    console.log("Reçu du composant enfant :", word)
+  }
+
+  function addWord() {
+    if (!translatedWord) return
+
+    const newWord = {
+      ...translatedWord,
+      createdAt: new Date().toISOString().split("T")[0],
+    }
+
+    setAddedWord((prevWords) => [...prevWords, newWord])
   }
   return (
     <div className="max-w-[900px] mx-auto pt-20 h-screen pb-5 flex flex-col gap-6">
       <section id="add">
-        <Add displayWord={handleTranslatedWord} />
+        <Add
+          displayWord={handleTranslatedWord}
+          onLoadingChange={setIsLoading}
+          addWord={addWord}
+        />
       </section>
-      {translatedWord && <WordCard {...translatedWord} status="pending" />}
+      {isLoading ? (
+        <CardSkeleton />
+      ) : (
+        translatedWord && <WordCard {...translatedWord} status="pending" />
+      )}
+      {/* display recently added Word */}
+      {addedWord.length > 0 && (
+        <section className="mt-8">
+          <h3 className="text-lg font-bold mb-4">Mots ajoutés récemment :</h3>
+          <div className="flex flex-col gap-4">
+            {addedWord
+              .slice(-3)
+              .reverse()
+              .map((word) => (
+                <WordCard key={word.id} {...word} />
+              ))}
+          </div>
+        </section>
+      )}
     </div>
   )
 }
