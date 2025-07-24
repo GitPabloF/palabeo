d0e80e8b8f348232f1288f396b3321e9b23a01d6
PabloF <pablofitoussi@gmail.com>
2025-07-24 14:48:08 +0200

    prisma user and word schema created

---
+++ prisma/migrations/20250724121553_init/migration.sql
@@ -0,0 +1,48 @@
+-- CreateEnum
+CREATE TYPE "LangCode" AS ENUM ('fr', 'es', 'en', 'de', 'it', 'pt');
+
+-- CreateEnum
+CREATE TYPE "WordTypeCode" AS ENUM ('adj', 'nf', 'nm', 'vi', 'vt', 'adv', 'pron');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Word" (
+    "id" SERIAL NOT NULL,
+    "wordFrom" TEXT NOT NULL,
+    "wordTo" TEXT NOT NULL,
+    "exampleFrom" TEXT NOT NULL,
+    "exampleTo" TEXT NOT NULL,
+    "langFrom" TEXT NOT NULL,
+    "langTo" TEXT NOT NULL,
+    "typeName" TEXT NOT NULL,
+    "typeCode" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    "lastReviewed" TIMESTAMP(3),
+    "reviewCount" INTEGER NOT NULL DEFAULT 0,
+    "mastered" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Word_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Word_userId_langFrom_langTo_idx" ON "Word"("userId", "langFrom", "langTo");
+
+-- CreateIndex
+CREATE INDEX "Word_userId_typeCode_idx" ON "Word"("userId", "typeCode");
+
+-- AddForeignKey
+ALTER TABLE "Word" ADD CONSTRAINT "Word_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
