89a5615fdc5c5c53dd3218903b9ae859270cd781
PabloF <pablofitoussi@gmail.com>
2025-08-04 17:40:08 +0200

    new app Layout bg color

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,5 +1,5 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import AppSidebar from "@/app/app/components/appSidebar"
+import AppSidebar from "@/components/block/appSidebar"
 import { UserProvider } from "@/contexts/UserContext"
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
@@ -7,7 +7,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     <UserProvider>
       <SidebarProvider>
         <AppSidebar />
-        <main className="relative bg-gray-100 min-h-screen w-screen flex-1 flex">
+        <main className="relative bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 min-h-screen w-screen flex-1 flex">
           <div className="flex-1  min-h-screen pb-5 flex flex-col gap-6 mx-10 py-8">
             <SidebarTrigger />
             <div className="max-w-[900px] w-full items-center flex-1 self-center">
